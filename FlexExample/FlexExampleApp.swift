d46d7a7eafeee147f00b559bd4cf9039e7153740
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-12-04 14:51:25 +0900

    Initial Commit

---
+++ FlexExample/FlexExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexExampleApp.swift
+//  FlexExample
+//
+//  Created by 柳元 俊輝 on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct FlexExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
