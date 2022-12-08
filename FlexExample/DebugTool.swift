10e8958c665150dcc97a9c2fb80fd6571e74aae3
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-12-08 22:17:20 +0900

    mod

--- FlexExample/DebugTool.swift
+++ FlexExample/DebugTool.swift
@@ -6,13 +6,19 @@
 //
 
 #if DEBUG
-import Combine
 import FLEX
+import SwiftUI
+import Combine
 
 final class DebugTool {
     private var subscriptions = Set<AnyCancellable>()
 
     func setupFlex() {
+        FLEXManager.shared.registerGlobalEntry(withName: "SmpleViewに画面遷移") { hostViewController in
+            let controller = UIHostingController(rootView: SampleView())
+            hostViewController.present(controller, animated: true)
+        }
+
         FLEXManager.shared.registerGlobalEntry(withName: "GithubAPIを叩く") { _ in
             GithubAPI.searchRepos(page: 1, perPage: 30)
                 .sink(receiveCompletion: { completion in
---
+++ FlexExample/SampleView.swift
@@ -0,0 +1,20 @@
+//
+//  SampleView.swift
+//  FlexExample
+//
+//  Created by 柳元 俊輝 on 2022/12/08.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Sample View")
+    }
+}
+
+struct SampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SampleView()
+    }
+}
